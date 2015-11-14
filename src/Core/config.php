bab0b64dbf75c7cfcff5439a9bf35ebb00f5cd51
Akhyar Maulana <akhyarmaulana99@gmail.com>
2015-11-14 14:20:36 +0700

    git ignore config

--- src/Core/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-//Database Setting
-$config['dbhost'] = 'localhost';
-$config['dbuser'] = 'root';
-$config['dbpass'] = 'anisnuzulan123`';
-$config['dbname'] = 'dda';
-
-//Plates 
-$config['plates_dir'] = __DIR__.'/../Pangeran';
